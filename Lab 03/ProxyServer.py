6fab28f1afe5b7803c6459a00d01fdd974538a75
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2023-10-28 11:07:21 -0600

    Connected to Google (somehow)

--- Lab 03/ProxyServer.py
+++ Lab 03/ProxyServer.py
@@ -1,74 +1,56 @@
 from socket import *
-import sys
+from urllib.parse import urlparse
 
-if len(sys.argv) <= 1:
-    print('Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server')
-    sys.exit(2)
-
-# Create a server socket, bind it to a port and start listening
-tcpSerSock = socket(AF_INET, SOCK_STREAM)
-
+PROXY_PORT = 8888
 server_host = 'localhost'
-# You need to bind the server socket to an IP address and port here
-tcpSerSock.bind(('your_ip_address', 8888))
-# Start listening on the server socket
-tcpSerSock.listen(1)
 
-while True:
-    # Start receiving data from the client
-    print('Ready to serve...')
-    tcpCliSock, addr = tcpSerSock.accept()
-    print('Received a connection from:', addr)
+def handle_request(client):
+    message = client.recv(4096).decode('utf-8')
 
-    message = tcpCliSock.recv(4096).decode()
+    print("Message: ", message)
 
+    lines = message.split('\r\n')
+    if len(lines) == 0:
+        return
 
-    filename = message.split()[1].partition("/")[2]
-    fileExist = "false"
-    filetouse = "./Lab 03/" + filename
+    line1 = lines[0]
+    print("first line: ", line1)
+    method, url, http = line1.split(' ')
+
+    if method != 'GET':
+        respose = b'HTTP/1.1 400 Bad Request\r\n\r\n'
+        client.sendall(respose)
+        return
 
     try:
-        # Check whether the file exists in the cache
-        f = open(filetouse[1:], "r")
-        outputdata = f.readlines()
-        fileExist = True
+        server = socket(AF_INET, SOCK_STREAM)
+        host = url.strip('/')
+        server.connect((host, 80))
 
-        # Send a response message from cache
-        tcpCliSock.send("HTTP/1.0 200 OK\r\n")
-        tcpCliSock.send("Content-Type:text/html\r\n")
+        server.sendall(message.encode())
 
-        # You need to send the cached data back to the client here
-        print('Hit Cache')
+        response = server.recv(4096)
 
-    except IOError:
-        if fileExist == False:
-            # Create a socket on the proxy server
-            c = socket(AF_INET, SOCK_STREAM)
-            hostn = filename.replace("www.", "", 1)
+        client.sendall(response)
 
-            try:
-                # Connect to the socket to port 80
-                # Example: c.connect((hostn, 80))
-
-                # Create a temporary file on this socket and send the request to port 80
-                fileobj = c.makefile('r', 0)
-                fileobj.write("GET " + "http://" + filename + "HTTP/1.0\n\n")
-
-                # You need to read the response into a buffer and send it back to the client
-
-                # Create a new file in the cache for the requested file
-                # Also, send the response in the buffer to the client socket and the corresponding file in the cache
-                tmpFile = open("./" + filename, "wb")
-
-                # You need to write the data to the cache and send it back to the client
-
-            except:
-                print("Illegal request")
-
-    # Handle HTTP response message for file not found
+    except Exception as e:
+        print('oof Error: ', e)
+    
 
     # Close the client and the server sockets
-    tcpCliSock.close()
+    client.close()
+    server.close()
 
-# Close the server socket
-# Example: tcpSerSock.close()
+
+def main():
+    proxy_socket = socket(AF_INET, SOCK_STREAM)
+    proxy_socket.bind((server_host, PROXY_PORT))
+    proxy_socket.listen(1)
+
+    while True:
+        client, addr = proxy_socket.accept()
+        print("Connection from", addr)
+        handle_request(client)
+
+if __name__ == '__main__':
+    main()
